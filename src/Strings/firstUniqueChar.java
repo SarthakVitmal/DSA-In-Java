afdef8411f4228b43591fa4fea5ec18cf9c87679
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-03-25 00:52:05 +0530

    solved leetcode 387

--- src/Strings/firstUniqueChar.java
+++ src/Strings/firstUniqueChar.java
@@ -1,22 +1,26 @@
 package Strings;
 
+import java.util.HashMap;
+
 public class firstUniqueChar {
     public static void main(String[] args) {
-        String s = "loveleetcode";
-        char[] ch = s.toCharArray();
-        int nonRepeatIdx = -1;
-        int i = 0;
-
-        while (i < ch.length - 1) {
-            int j = i + 1;
-            if (ch[i] == ch[j]) {
-                i++;
-            } else {
-                nonRepeatIdx = i;
-                break;
-            }
-        }
-
+        String s = "leetcode";
+        int nonRepeatIdx = firstUniqChar(s);
         System.out.println(nonRepeatIdx);
+    }
+
+    private static int firstUniqChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(map.containsKey(s.charAt(i))){
+                int freq = map.get(s.charAt(i));
+                map.put(s.charAt(i),freq+1);
+            }else map.put(s.charAt(i),1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            int freq = map.get(s.charAt(i));
+            if(freq == 1)return i;
+        }
+        return -1;
     }
 }
