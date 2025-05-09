b304b9a73b928c77f4684a80b1cf7fa9e55d5edc
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-05-09 22:28:26 +0530

    solved leetcode 2418

--- src/Arrays/sortThePeople.java
+++ src/Arrays/sortThePeople.java
@@ -1,22 +1,34 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class sortThePeople {
     public static String[] sortPeople(String[] names,int[] heights){
         String[] ans = new String[names.length];
         HashMap<String,Integer> map = new HashMap<>();
+        Object[][] people = new Object[names.length][2];
 
         for (int i = 0; i < names.length; i++) {
             map.put(names[i],heights[i]);
+            people[i][0] = heights[i];
+            people[i][1] = names[i];
         }
-
-        System.out.println(map);
+        Arrays.sort(people, (a, b) -> ((Integer) b[0] - (Integer) a[0]));
+        for (int i = 0; i < people.length; i++) {
+            for (int j = 0; j < people[0].length; j++) {
+                ans[i] = (String) people[i][1];
+            }
+        }
         return ans;
     }
     public static void main(String[] args) {
         String[] names = {"Mary","John","Emma"};
         int[] height = {180,165,170};
         String[] res = sortPeople(names,height);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
     }
 }
