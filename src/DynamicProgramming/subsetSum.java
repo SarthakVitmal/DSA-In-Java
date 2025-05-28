8c1d4e85977d3fc99b326f34af2f5294334cccb2
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-05-28 20:39:42 +0530

    solved subset problem

--- src/DynamicProgramming/subsetSum.java
+++ src/DynamicProgramming/subsetSum.java
@@ -1,18 +1,34 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class subsetSum {
     public static void main(String[] args) {
         int[] arr = {0,8,5,2,4};
         int target = 15;
-        boolean res = isSubsetSum(0,arr,target);
+        int[][] dp = new int[arr.length][target+1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        boolean res = isSubsetSum(0,arr,target,dp);
         System.out.println(res);
     }
 
-    private static boolean isSubsetSum(int idx, int[] arr, int target) {
+    private static boolean isSubsetSum(int idx, int[] arr, int target, int[][] dp) {
         if(target == 0)return true;
         if(idx == arr.length)return false;
-        boolean skip = isSubsetSum(idx+1,arr,target);
-        boolean take = isSubsetSum(idx + 1, arr, target - arr[idx]);
-        return skip || take;
+        if(dp[idx][target] != -1) return (dp[idx][target] == 1);
+        boolean ans;
+        boolean skip = isSubsetSum(idx+1,arr,target,dp);
+        if(target -arr[idx] < 0) ans = skip;
+        else{
+            boolean take = isSubsetSum(idx + 1, arr, target - arr[idx], dp);
+            ans = take || skip;
+        }
+        if(ans) dp[idx][target] = 1;
+        else dp[idx][target] = 0;
+        return ans;
     }
 }
