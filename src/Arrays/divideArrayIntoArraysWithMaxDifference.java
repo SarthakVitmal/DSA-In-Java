94782c8701c9a0ee6bec77a67f05560b88d144dc
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-06-18 12:12:16 +0530

    solved leetcode 2966

--- src/Arrays/divideArrayIntoArraysWithMaxDifference.java
+++ src/Arrays/divideArrayIntoArraysWithMaxDifference.java
@@ -17,24 +17,23 @@ public class divideArrayIntoArraysWithMaxDifference {
 
     private static int[][] divideArray(int[] arr, int k) {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        int[][] res2 = new int[0][0];
-        ArrayList<Integer> al;
+        int[][] empty = new int[0][0];
+        ArrayList<Integer> al = new ArrayList<>();
         Arrays.sort(arr);
         int i = 0;
-        al = new ArrayList<>();
         while (i < arr.length){
             al.add(arr[i]);
             if((i+1) % 3 == 0){
-                if((al.get(0) - al.get(1) <= k && al.get(1) - al.get(0) <= k) && (al.get(1) - al.get(2) <= k && al.get(2) - al.get(1) <= k) && (al.get(0) - al.get(2) <= k && al.get(2) - al.get(0) <= k)){
-                    ans.add(al);
-                    al = new ArrayList<>();
-                }else return res2;
+                if(al.get(2) - al.get(0) <= k){
+                    ans.add(new ArrayList<>(al));
+                    al.clear();
+                }else return empty;
             }
             i++;
         }
-        int[][] res = new int[ans.size()][ans.get(0).size()];
+        int[][] res = new int[ans.size()][3];
         for (int j = 0; j < ans.size(); j++) {
-            for (int l = 0; l < ans.get(j).size(); l++) {
+            for (int l = 0; l < 3; l++) {
                 res[j][l] = ans.get(j).get(l);
             }
         }
