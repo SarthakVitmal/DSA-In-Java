9d818ead94a51c9755deb5e4e70ea65dc50d38a1
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-07-11 11:30:38 +0530

    solved leetcode 5

--- src/Strings/longestPalindromeSubstring.java
+++ src/Strings/longestPalindromeSubstring.java
@@ -1,33 +1,30 @@
 package Strings;
 
 public class longestPalindromeSubstring {
-    public static String longestPalindrome(String s){
-        StringBuilder sb;
-        StringBuilder ans = new StringBuilder();
-        int n = s.length() - 1;
-        int i = 0; int j = n;
-        int maxLen = 0;
-        if(s.length() == 1)return s;
-        while (i < j){
-            int length = 0;
-            sb = new StringBuilder();
-            for (int k = i; k <= j; k++) {
-                sb.append(s.charAt(k));
-                length++;
-            }
-            if(sb.toString().contentEquals(sb.reverse())){
-                maxLen = Math.max(length,maxLen);
-                System.out.println(ans);
-                ans = sb;
-            }
-            i++;
-            j--;
-        }
-        return ans.toString();
-    }
     public static void main(String[] args) {
-        String s = "ac";
+        String s = "babad";
         String ans = longestPalindrome(s);
         System.out.println(ans);
+    }
+    private static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    private static String longestPalindrome(String s) {
+        String maxLen = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String sub = s.substring(i,j+1);
+                if(isPalindrome(sub) && sub.length() > maxLen.length()){
+                    maxLen = sub;
+                }
+            }
+        }
+        return maxLen;
     }
 }
